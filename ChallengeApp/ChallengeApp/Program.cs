ded2a6328d1e2ea98ef97dac58952c1b57aa7e6c
Szejki <szejki16@wp.pl>
2023-03-09 22:00:43 +0100

    Zadanie domowe - Dzień: 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Łukasz";
+bool sex = true;
+int age = 35;
+string resultSex = string.Empty;
+
+
+if (sex == true)
+{
+    resultSex = "mężczyzną.";
+}
+else if (sex == false)
+{
+    resultSex = "kobietą.";
+}
+if (sex == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nazywasz się " + name + ". Masz " + age + " lat i jesteś " + resultSex);
+}
